8bcba8d59d049991b2c483527e010ebd4a65dc64
Daniel Bannert <d.bannert@anolilab.de>
2018-07-11 00:18:40 +0200

    fixed the exception class and moved to common

---
+++ Contract/Exception/InvalidArgumentException.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types=1);
+namespace Narrowspark\Discovery\Common\Contract\Exception;
+
+use InvalidArgumentException as BaseInvalidArgumentException;
+
+class InvalidArgumentException extends BaseInvalidArgumentException implements Exception
+{
+}
